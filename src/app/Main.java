821fc60c5bd2bab030f54fb024bc7d42931f3e29
Egor Voroshilov <163694103+Walkthroughcoding@users.noreply.github.com>
2024-12-07 17:46:19 +0300

    Revert "Added functionality according to assignment specifications"

--- src/app/Main.java
+++ src/app/Main.java
@@ -36,9 +36,5 @@ public class Main {
         for (Subtask subtask : taskManager.getSubtasksOfEpic(epic1.getId())) {
             System.out.println(subtask);
         }
-            System.out.println("История задач:");
-            for (Task task : taskManager.getHistory()) {
-                System.out.println(task);
-            }
-        }
     }
+}
